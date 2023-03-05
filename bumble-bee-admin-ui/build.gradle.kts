ad60dd9f0257b71ddec4dcbc2367b6b529eac8e5
Hasaranga Prasad <hasaranga1234.prasad@gmail.com>
2023-03-05 11:21:36 +0530

    Add new module for UI implementation

---
+++ bumble-bee-admin-ui/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "com.bumble.bee"
+version = "1.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
